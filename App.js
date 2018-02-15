2be5fa62b5b6ffc2795f74af238fe94b895c0507
cedrvanh <cedrvanh@student.arteveldehs.be>
2018-02-15 19:58:40 +0100

    Moved directories

--- App.js
+++ App.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { View } from 'react-native';
 import { Text } from 'react-native-elements';
 
-import { Colors } from './utils/colors';
-import { ScanContent } from './components/ScanContent';
+import { Colors } from './src/utils/colors';
+import { ScanContent } from './src/components/ScanContent';
 
 export default class App extends React.Component {
-
   render() {
     return (
       <ScanContent />
