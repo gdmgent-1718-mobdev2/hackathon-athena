29064183d9f35610f50fb433e6a7b150a4bfb827
remysiri <remysiri@student.arteveldehs.be>
2018-02-16 04:38:08 +0100

    Added Popup Modal on scanPage and top navigation back button

--- src/components/ScanContent.js
+++ src/components/ScanContent.js
@@ -1,19 +1,49 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Text } from 'react-native-elements';
+import { Text, Button, Modal, TextInput } from 'react-native-elements';
 
 import { Camera } from './Camera';
 import { Color } from '../utils/colors';
 import { styles } from '../utils/styles';
 
 export class ScanContent extends React.Component {
+    state = { modalVisible: false, };
+    openModal() { this.setState({modalVisible:true});}
+    closeModal() { this.setState({modalVisible:false});}
+    constructor(props) {
+        super(props);
+        this.state = { text: 'Voer uw serial key in...'};
+    }
+
     render(){
         return(
             <View style={styles.container}>
                 <Text h4 style={styles.subtitle}>GASMETER</Text>
                 <Text h3 style={styles.title}>SCAN BARCODE</Text>
                 <Camera />
-                <Text></Text>
+                <Button
+                    title="Voer handmatig code in"
+                    onPress={() => this.openModal()}
+                />
+
+                <Modal
+                    visible={this.state.modalVisible}
+                    animationType={'slide'}
+                    onRequestClose={() => this.closeModal()}
+                >
+                    <View style={styles.modalContainer}>
+                        <View style={styles.innerContainer}>
+                            <TextInput
+                                onChangeText={(text) => this.setState({text})}
+                                value={this.state.text}
+                            />
+                            <Button
+                                onPress={() => this.closeModal()}
+                                title="Sluiten"
+                            />
+                        </View>
+                    </View>
+                </Modal>
             </View>
         );
     }
--- src/views/ShopScreen.js
+++ src/views/ShopScreen.js
@@ -3,11 +3,13 @@ import { View, StyleSheet } from 'react-native';
 import { Text, Button, Avatar } from 'react-native-elements';
 import { Colors } from '../utils/colors';
 import { SkinCard } from '../components/SkinCard';
+import { TopNav } from '../components/TopNav';
 
 export class ShopScreen extends React.Component {
     render(){
         return(
             <View style={styles.tmpContainer}>
+            <TopNav />
                 <Text style={{ color:Colors.TEXT_GREEN, fontSize: 16, textAlign:'center' }}>Pimp uw plant</Text>
                 <SkinCard />
                 <SkinCard />
