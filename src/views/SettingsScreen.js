61c8aaa94c5914f0a58f014fbda612403ad185fe
remysiri <remysiri@student.arteveldehs.be>
2018-03-03 22:10:38 +0100

    Updated Dependencies

--- src/views/SettingsScreen.js
+++ src/views/SettingsScreen.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Text, List, ListItem } from 'react-native-elements';
+import { Text, List, ListItem, Overlay } from 'react-native-elements';
 
 import { styles } from '../utils/styles';
+import { BottomNav } from '../components/BottomNav';
 
 export class SettingsScreen extends React.Component {
+
+    state = {
+        modalVisible: false,
+      };
+    
+      setModalVisible(visible) {
+        this.setState({modalVisible: visible});
+      }
+
     render(){
         const list = [
             {
@@ -23,6 +33,20 @@ export class SettingsScreen extends React.Component {
 
         return(
             <View style={{ flex: 1 }}>
+
+
+
+<Overlay
+  isVisible={this.state.modalVisible}
+  windowBackgroundColor="rgba(255, 255, 255, .5)"
+  overlayBackgroundColor="red"
+  width="auto"
+  height="auto"
+>
+  <Text>Hello from Overlay!</Text>
+</Overlay>
+
+
                 <Text h2>Test</Text>
                 <List>
                 {
@@ -31,10 +55,14 @@ export class SettingsScreen extends React.Component {
                         key={i}
                         title={item.title}
                         leftIcon={{name: item.icon}}
+                        onPress={() => {
+                            this.setModalVisible(!this.state.modalVisible);
+                          }}
                     />
                     ))
                 }
                 </List>
+                <BottomNav navigation={this.props.navigation} />
             </View>
         );
     }
